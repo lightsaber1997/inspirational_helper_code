2a52a31461bbff2b402e44e3818be3c0ec71dbd0
lightsaber_notebook <bestornot04@naver.com>
2022-08-09 15:36:58 +0900

    ..

--- python_script/User.java
+++ python_script/User.java
@@ -1,23 +1,11 @@
-package com.example.demo.user;
-
-import java.util.List;
+package com.inspirational.userDetails;
 
 import org.apache.ibatis.annotations.Param;
 import org.springframework.stereotype.Repository;
 
+
 @Repository
 public interface CustomUserDetailsDAO {
-	public List<CustomUserDetails> selectUserDetailsList();
 	public CustomUserDetails selectUserByLoginId(
 			@Param("loginId") String loginId);
-	public void insertUser(
-			@Param("id") int id,
-			@Param("loginId") String loginId,
-			@Param("encryptedPassword") String encryptedPassword,
-			@Param("email") String email,
-			@Param("phoneNumber") String phoneNumber,
-			@Param("authority") String authority,
-			@Param("userNameInApp") String userNameInApp,
-			@Param("realName") String realName);
-	
 }
--- python_script/mapper_generator.py
+++ python_script/mapper_generator.py
@@ -174,4 +174,4 @@ def dao_to_mapper(java_code_file_name, table_name=""):
         f.write('<!DOCTYPE mapper PUBLIC "-//mybatis.org//DTD Mapper 3.0//EN" \n"http://mybatis.org/dtd/mybatis-3-mapper.dtd">'.encode('utf8'))
         f.write(xml_data_binary)
 
-dao_to_mapper("User.java")
+dao_to_mapper("User.java", "user")
