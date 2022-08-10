f845e4661ca7a96495669c5d00597ab149ec4397
DESKTOP-Q9KG21M\user <megait@gmail.com>
2022-08-10 18:45:34 +0900

    ...

--- sql_script/creation01.sql
+++ sql_script/creation01.sql
@@ -12,6 +12,7 @@ CREATE TABLE `user` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 
+
 CREATE TABLE `korea_sigudong` (
 	`id` int NOT NULL AUTO_INCREMENT PRIMARY KEY,
 	`si` varchar(50) NOT NULL,
@@ -33,3 +34,33 @@ CREATE TABLE `grade` (
     `createdAt` timestamp DEFAULT CURRENT_TIMESTAMP,
     `updatedAt` timestamp DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+
+CREATE TABLE `student_profile` (
+	`id` int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	`studentId` int NOT NULL,
+	`content` text NOT NULL,
+    `createdAt` timestamp DEFAULT CURRENT_TIMESTAMP,
+    `updatedAt` timestamp DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+
+CREATE TABLE `subject` (
+	`id` int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	`subjectName` varchar(50) NOT NULL,
+    `createdAt` timestamp DEFAULT CURRENT_TIMESTAMP,
+    `updatedAt` timestamp DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+insert into `subject`
+(`subjectName`)
+values
+("국어"),
+("수학"),
+("영어"),
+("과학"),
+("사회"),
+("컴퓨터"),
+("미술"),
+("체육"),
+("기타");
